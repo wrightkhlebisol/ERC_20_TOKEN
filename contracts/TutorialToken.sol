662f4d2c92579db7dce8cc1ce892bf923597c71e
Wright <calebogundiya@gmail.com>
2020-07-14 21:58:28 +0100

    Setup token contract variables

--- contracts/TutorialToken.sol
+++ contracts/TutorialToken.sol
@@ -3,5 +3,14 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract TutorialToken is ERC20 {
+    string public name = "CalebToken";
+
+    string public symbol = "CT";
+
+    uint8 public decimals = 2;
+
+    uint public INITIAL_SUPPLY = 12000;
+
     
+
 }
